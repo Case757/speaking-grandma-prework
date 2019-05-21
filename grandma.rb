5e63b962de0db3604dce8bc0b62a6ea93a903903
David Fellows <dfellows12@gmail.com>
2019-05-21 23:16:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,12 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN"
   end
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
